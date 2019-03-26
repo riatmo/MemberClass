b1f9d2eabeb2942efc24b4c88f0bb01126c8f495
Ragil Triatmojo <ragil@redpedia.com>
2019-03-26 14:47:41 +0700

    Latihan Member Class

---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objek
+            Taxi driverku = new Taxi();
+
+            //Properties value
+            driverku.DriverName = "Ragil";
+            driverku.OnDuty = true;
+            driverku.NumPassenger = 16;
+
+           
+        
+
+            //Call Method Taxi
+            driverku.TaxiInfo();
+            if (driverku.OnDuty == true)
+            {
+                driverku.PickUpPassenger();
+
+            }
+            else
+            {
+                driverku.DropOffPassenger();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LatihanMemberClass/Taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Taxi
+    {
+        //Properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+        
+
+        //Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty ? "Yes" : "No");
+            Console.WriteLine("Number of Passenger : {0}", NumPassenger);
+
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n\nDriver {0} sedang sibuk", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("\n\nDriver {0} tersedia", DriverName);
+        }
+
+    }
+}
